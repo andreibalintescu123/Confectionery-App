893e3216b974e7a3b9cf066032bef91fd76221d4
Balintescu <92201004+andreibalintescu123@users.noreply.github.com>
2025-01-02 17:39:55 +0200

    Added the in-memory repository. Established a starting point for the application. Added a few custom exceptions to the Exception folder.

---
+++ src/main/java/org/confectionery/Exception/ValidationException.java
@@ -0,0 +1,7 @@
+package org.confectionery.Exception;
+
+public class ValidationException extends RuntimeException {
+    public ValidationException(String message) {
+        super(message);
+    }
+}
